6545f857e34f1e60cef270e716878feb6dad3ce7
Yan Hill <yanhill@live.com>
2020-04-24 21:26:46 -0300

    Update lotus_2.js

--- lotus_2.js
+++ lotus_2.js
@@ -131,6 +131,33 @@ function setup() {
     }
 }
 
+window.addEventListener('orientationchange', function(){
+    resizeCanvas(windowWidth, windowHeight);
+
+    centroX = windowWidth/2;
+    if(windowWidth/windowHeight < 0.6){
+        centroY = windowHeight/2.5;
+    }
+    else{
+        centroY = windowHeight/2.25;
+    }
+    if(aboutOnScreen){
+        aboutText.style.top = centroY-(aboutText.clientHeight/2)+"px";
+        aboutButtons.style.width = windowWidth - btnUpdate.offsetWidth - 20;
+    }
+
+    if(windowWidth/windowHeight > 1){
+        containerInicio.style.top = centroY-(containerInicio.clientHeight/2)+"px";
+        containerInicio.style.left = (centroX-containerInicio.clientWidth-deslocamento-80)+"px";
+    }
+    else if(windowWidth/windowHeight < 1){
+        containerInicio.style.top = centroY-(containerInicio.clientHeight/2)-(deslocamento+100)+"px";
+        containerInicio.style.left = "auto";
+    }
+
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
     btnRestart = document.getElementById("restart");
     btnUpdate = document.getElementById("update");
