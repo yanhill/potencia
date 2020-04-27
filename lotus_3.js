814badede44a9c41fd095c8e1bbef91835ff1323
Yan Hill <yanhill@live.com>
2020-04-27 00:47:34 -0300

    Update lotus_3.js

--- lotus_3.js
+++ lotus_3.js
@@ -1,211 +1,145 @@
-var x2;
-var y2;
+var aboutWindow, aboutText;
+var aboutOnScreen, contactOnScreen;
+var title;
+var aboutButtons;
+var tela;
+var mesaInf, mesaSup; 
+var containerInicio;
+var btnUpdate, btnRestart, about, contact;
+var canvasScript;
 
-var t;
-
-var vetores;
-var afastamento;
-var reparticao;
-var raioX;
-var raioY;
-var deformaX;
-var deformaY;
-
-var centroX;
-var centroY;
-
-var velocidadeParticulas;
-var deslocamento;
-
-var comecou, movendo, primeiro;
-
-var amplificador;
-
-var hueA,hueB,hueC,hueD,hueE;
-
-var inicio, fim, entremeio;
-
-function drawParticle(x,y,w,h,hue,sat,bri){
-    fill(hue,sat,bri);
-    rect(x,y,w,h);
-}
-
-window.addEventListener("load",function() {
-    setTimeout(function(){
-        window.scrollTo(0, 1); // This hides the address bar:
-    }, 0);
-});
-
-function predefinicoesDoObjeto(){
-    vetores = 100;
-    afastamento = 500;
-    deslocamento = random(40,60);
-    reparticao = 20;
-    raioX = -42;
-    raioY = -12; 
-    deformaX = 126.85337;
-    deformaY = 319.79946;
-
-    x2 = function(t) {
-        return raioX * sin(t / deformaX);};
-    y2 = function(t) {
-        return raioY * sin(t / deformaY);};
-
-    centroX = window.outerWidth/2;
-    if(window.outerWidth/window.outerHeight < 0.6){
-        centroY = window.outerHeight/2.3;
-    }
-    else{
-        centroY =  window.outerHeight/2.2;
-    }
-  }
-
-function setup() {
-    createCanvas(window.outerWidth,  window.outerHeight);
-    background(200,200,255);
-    frameRate(25);
-    colorMode(HSB,360,100,100,1);
-    song.stop();
-    song.setVolume(1);
-
-    t = 0;
-
-    velocidadeParticulas= 3; 
-    maxVelocity = 20;
-
-    comecou = false;
-    primeiro = true;
-    aboutOnScreen = false;
-    contactOnScreen = false;
-
-    predefinicoesDoObjeto();
-    predefinicoesDoSom();
-    //criaSliders();
-
-    tela = document.getElementById("defaultCanvas0");
-    tela.addEventListener('mouseup', touchScreen, false);
-
-    canvasScript = document.getElementById("defaultCanvas0");
-
-
-    gameBack();
-}
-function windowResized() {
-    resizeCanvas(window.outerWidth,  window.outerHeight);
-    realignCenter();
-    if(!comecou){
-        realignBegining();
-    }
-}
-
-window.addEventListener("resize", function(){
-    console.log();
-    resizeCanvas(window.outerWidth,  window.outerHeight);
-    realignCenter();
-    if(!comecou){
-        realignBegining();
-    }
-});
-
-
-function draw(){
-    background(200 , 100 , 100);
-    //assignSliders();
-    comecaCalcularMedia();
-    inputa();
-    gravidade();
-
-    push();
-    translate(centroX,centroY);
-    objeto();
-    pop();
-}
-function gravidade(){
-    var easing = 0.0009;
-    var target = 9000;
-
-    if (comecou && primeiro){
-        var dx = target - raioX;
-        var dy = target - raioY;
-
-        raioX += dx * easing;
-        raioY += dy * easing;
-        console.log("merda_primeiro")
-        //console.log(raioX)
-    }
-    else if(comecou && !primeiro){
-        raioX+=1;
-        raioY+=1;
-    }
-}
-function objeto(){
-    inicio = 0.05;
-    fim = 0.9;
-    entremeio = 1.8;
+document.addEventListener('DOMContentLoaded', function() {
+    btnRestart = document.getElementById("restart");
+    btnUpdate = document.getElementById("update");
+    about = document.getElementById("about");
+    aboutWindow = document.getElementById("aboutWindow");
+    mesaSup = document.getElementById("mesaSup");
+    containerInicio = document.getElementById("inicio");
+    title = document.getElementById("title");
+    aboutText = document.getElementById("aboutText");
+    aboutButtons = document.getElementById("aboutButtons");
+    contact = document.getElementById("contact");
+    mesaInf = document.getElementById("mesaInf");
     
-    t+=velocidadeParticulas;
-    strokeWeight(1);
-    for(var vao = 0, createdVectors = 0, spectrumPosition = 0; createdVectors <= vetores;  createdVectors++){
-        var contador = 0;
-        
-        if( spectrumPosition == 25){
-            spectrumPosition = 0;
-        }
-        
-        rotate(PI/reparticao);
-        for(var a = inicio; a <= fim; a *= entremeio){
 
-            var posiX = x2(t+vao);
-            var posiY = y2(t+vao);    
-            
-            if(contador == 0){
-                stroke(0,100,100);
-                drawParticle(-posiX*(a/20) , -posiY*(a/20) , -posiY*(a/8), -posiX*(a/8) ,0,100,80);
-                stroke(30 , 0, 100);
-                drawParticle(-posiX*(a/2) , -posiY*(a/2) , -posiY*(a/3) , -posiX*(a/3) , 30 , 0, 100);
-               
-                stroke(300 , 50 , 100);
-                drawParticle((posiX*a)+deslocamento , (posiY*a)+deslocamento , 3+(a/4)*(posiX) , 6+(a/4)*posiY , 300 , 30 , 100);
-            }
-      
-            else if(contador == 1) {
-                stroke(60 , 100 , 100);
-                drawParticle((posiX*a)+deslocamento , (posiY*a)+deslocamento , 3+(a/4)*(posiX) , 6+(a/4)*posiY ,50 , 100 , 100);
-            }           
-            else if(contador == 2){  
-                stroke(80 , 100 , 100);
-                drawParticle((posiX*a)+deslocamento , 1+(posiY*a)+deslocamento , 5+(a/16)*(posiX/5) , 5+(a/16)*posiY/5 ,100 , 100 , 90);
-            }
-                
-            else if(contador == 3){
-                stroke(310 , 80 , 100);
-                drawParticle((posiX*a)+deslocamento , (posiY*a)+deslocamento , 3+(a/4)*(posiX) , 6+(a/4)*posiY ,   310 , 55 , 100);
-            }   
-            else if(contador == 4){
-                stroke(270 , 80 , 100);
-                drawParticle((posiX*a)+deslocamento , (posiY*a)+deslocamento , 6+(a/4)*(posiX) , 3+(a/4)*posiY , 280 , 65 , 100);
-            }         
-            desenharAgulhas(posiX,posiY,spectrumPosition,a);
-            contador++;
-            vao += afastamento;
-            spectrumPosition++;
-        }
-    }    
+    btnRestart.addEventListener('click', restart, false);
+    btnUpdate.addEventListener('click', update, false);
+    contact.addEventListener('click', showContact, false);
+    about.addEventListener('click', showAbout, false);
+    aboutWindow.addEventListener('click',hideAbout , false);
+ }, false); 
+
+
+ function update(){};
+ function restart(){
+    comecou = false;
+    primeiro = true;  
+
+    console.log("restart");
+    song.stop();
+    predefinicoesDoObjeto();
+
+    if(aboutOnScreen){
+        hideAbout();
+    }       
+    containerInicio.style.display="block";
+    btnRestart.style.display="none";
+ }
+
+
+ function pauseGame(){
+    aboutWindow.style.display ="block";
+    about.style.zIndex="1";
+    about.innerHTML="Toque para voltar";   
+    containerInicio.style.display="none";
+    mesaInf.style.background="none";
+    contact.innerHTML="";
+    title.style.display="none";
+ }
+
+
+ function gameBack(){
+    mesaSup.style.display="block";
+    about.style.display="flex";
+    about.innerHTML="sobre";  
+    contact.innerHTML="contato";
+    aboutWindow.style.display ="none"; 
+    title.style.display="block";
+    mesaInf.style.background="RGBA(0,0,0,0.5)";
+    aboutText.innerHTML ="";
+    aboutText.style.display ="block";
+
+    if(!comecou){
+        btnRestart.style.display="none";
+        containerInicio.style.display="block";
+        realignBegining();     
+    }
 }
-function desenharAgulhas(posiX,posiY,spectrumPosition,a){
-    if(spectrumPosition <= 5){
-        amplificador = map(spectrum[spectrumPosition],0,256,1,75); 
-    }
-    else if(spectrumPosition <= 10){
-        amplificador = map(spectrum[spectrumPosition],0,256,0,200);
-    }
-    else if(spectrumPosition <= 20){
-        amplificador = map(spectrum[spectrumPosition],0,256,0,300);
-    }
-    else{
-        amplificador = map(spectrum[spectrumPosition],0,256,0,400);
-    }
-    strokeWeight(0.5);
-    stroke(300,50,100)
-    drawParticle(-posiX*(a), -posiY*(a), 1.5 , 1.5*(amplificador) ,0,100,100);
+
+
+ function showContact(){
+    contactOnScreen = true;
+    pauseGame();
+    aboutButtons.style.display = "none";
+    mesaSup.style.zIndex="0";
+    aboutText.innerHTML ="Ambiente sensível ao som e ao toque, produzido com javascript e P5.js por Yan Hill.";
+    aboutText.style.top = centroY-(aboutText.clientHeight/2)+"px";
+    mesaSup.style.display = "none";
+ }
+
+
+ function showAbout(){   
+    aboutOnScreen = true;
+    pauseGame();
+    aboutButtons.style.display = "block";
+    mesaSup.style.zIndex="1";
+    btnRestart.style.display="block";    
+    aboutText.style.top = centroY-(aboutText.clientHeight/2)+"px";
+    aboutButtons.style.width = windowWidth - btnUpdate.offsetWidth - 20;
 }
+
+
+ function hideContact(){
+    gameBack();
+    aboutButtons.style.display = "block"; 
+    contactOnScreen = false;
+}
+
+
+function hideAbout(){
+    gameBack();  
+    aboutOnScreen = false;
+}
+
+
+function realignBegining(){   
+    if(windowWidth/windowHeight > 1){ // HORIZONTAL
+        containerInicio.style.top = centroY-(containerInicio.clientHeight/2)+"px";
+        containerInicio.style.left = centroX-(containerInicio.clientWidth/2)-(deslocamento+150)+"px"}
+    else{ // VERTICAL
+        containerInicio.style.top = centroY-(containerInicio.clientHeight/2)-(deslocamento+100)+"px";
+        containerInicio.style.left = (centroX-(containerInicio.clientWidth*0.5))+"px";}
+    }
+
+
+function realignCenter(){
+    centroX = windowWidth/2;
+    if(windowWidth/windowHeight < 0.6){
+        centroY = windowHeight/2.5;}
+    else centroY = windowHeight/2.25;
+
+    if(aboutOnScreen || contactOnScreen){
+        aboutText.style.top = centroY-(aboutText.clientHeight/2)+"px";
+        aboutButtons.style.width = windowWidth - btnUpdate.offsetWidth - 20;
+    }
+}
+
+
+window.addEventListener('orientationchange', function(){
+    resizeCanvas(windowWidth, windowHeight);
+    console.log("orientacao_mudou");
+    realignCenter();
+    if(!comecou){
+        realignBegining();
+    }
+});
